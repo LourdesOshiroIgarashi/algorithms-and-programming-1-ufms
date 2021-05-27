4d0286d0c4e47e8aebd800bee108e8b28cc39a60
vitor.lameirao <84359037+VitorLameirao@users.noreply.github.com>
2021-05-27 20:26:30 -0300

    Delete Lists/Lista 1/Vitor directory

--- Lists/Lista 1/Vitor/Ex1.py
+++
@@ -1,6 +0,0 @@
-#Entrada
-lista = list(map(float,input("Digite 10 números reais: ").split(" ")))
-#Processamento
-for i in range(9, -1 , -1):
-    print(lista[i], end = ' ')
-
--- Lists/Lista 1/Vitor/Ex2.py
+++
@@ -1,9 +0,0 @@
-#Entrada
-notas = list(map(float,input("Digite suas quatro notas: ").split(" ")))
-#Processamento
-soma = 0 
-for i in notas:
-    soma = soma + i
-media = soma/len(notas)
-#Saída
-print("Notas: {} \nMédia: {}".format(notas,media)) 
