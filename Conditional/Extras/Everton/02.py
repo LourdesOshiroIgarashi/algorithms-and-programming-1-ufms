ca2c9cefa9eb86a7cbc80add01ed8d4a4f5c2104
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-05-27 17:15:53 -0400

    Create 02.py

---
+++ Conditional/Extras/Everton/02.py
@@ -0,0 +1,10 @@
+'''Considere o problema de computar o valor absoluto de um número real.
+O valor absoluto de um número real x é dado por f(x) = x se x >= 0 ou f(x) = -x se x < 0.
+Projete e implemente um programa em Python que lei um número de ponto flutuante x, calcule e imprima o valor absoluto de x.'''
+
+x = float(input())
+
+y = (x**2)**(1/2)
+
+
+print("|{:.2f}| = {:.2f}".format(x,y))
