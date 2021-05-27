18ab9ff7fa7e0841f1938f166d2a53e0957f9de3
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-05-27 16:52:42 -0400

    EXERCÍCIO 8

---
+++ Conditional/Lista 1/Everton/08.py
@@ -0,0 +1,11 @@
+renda = float(input())
+if renda > 3582.00:
+  print("27.5")
+elif 1866.71 <= renda <= 3582.00:
+  print("22.5")
+elif 2150.01 <= renda <= 2866.70:
+  print("15.0")
+elif 1434.60 <= renda <= 2150.00:
+  print("7.8")
+else:
+  print("Isento")
