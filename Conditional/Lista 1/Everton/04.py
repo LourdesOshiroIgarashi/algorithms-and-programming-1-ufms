ed8191a43c1a47ca0a4f58bbd6a426337dda8500
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-05-27 16:50:07 -0400

    EXERCÍCIO 4

---
+++ Conditional/Lista 1/Everton/04.py
@@ -0,0 +1,19 @@
+x = float(input())
+
+if x/1 == int(x):
+  print("É inteiro.")
+else: 
+  print("Não é inteiro.")
+  
+  
+  #or
+  
+  # inteiro
+
+x = float(input())
+
+if x % 1 == 0:
+    print("É inteiro")
+
+else:
+    print("Não é inteiro")
