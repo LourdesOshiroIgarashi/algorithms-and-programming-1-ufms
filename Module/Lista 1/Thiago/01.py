907f1d073726a27d58096df3a75d078523be09f4
lutzzdias <80894260+lutzzdias@users.noreply.github.com>
2021-05-27 16:38:13 -0300

    pasta módulo, lista 1, minha pasta, ex. 1

---
+++ Module/Lista 1/Thiago/01.py
@@ -0,0 +1,13 @@
+# Leia dois valores inteiros a e b informados pelo usuário e determine o valor de
+# a + b. O cálculo de a + b deve ser realizado por uma função que recebe estes dois
+# valores inteiros como parâmetro.
+
+def soma(a, b):
+    return a + b
+
+
+n1 = int(input())
+n2 = int(input())
+soma = soma(n1, n2)
+
+print(soma)
