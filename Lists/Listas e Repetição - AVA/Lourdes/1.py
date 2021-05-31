034a6aee68930ded081532553b55ba139dc92ddf
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-05-31 17:33:54 -0400

    Create 1.py

---
+++ Lists/Listas e Repetição - AVA/Lourdes/1.py
@@ -0,0 +1,7 @@
+x = []
+
+for i in range(1):
+    x = list(map(int, input().split(" ")))
+
+print(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], "\n")
+print(x[9], x[8], x[7], x[6], x[5], x[4], x[3], x[2], x[1], x[0])
