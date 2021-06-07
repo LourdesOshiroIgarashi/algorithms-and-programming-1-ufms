2a80d21f1730d363374748a320bb1af6ad0bdbd0
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-06-07 12:13:33 -0400

    Create 13.py

---
+++ Lists/Listas e Repetição - AVA/Everton/13.py
@@ -0,0 +1,13 @@
+k = int(input())
+lista = []
+soma = 0
+for i in range(1, k+1, 1):
+  var = 5*i-5-i**2
+  lista.append(var)
+  soma = soma + var
+print('Sequência:')
+for i in range(0, len(lista), 1):
+  print('{}'.format(lista[i]), end=' ')
+
+print('\n')
+print('Para k = {}, o valor do somatório é: {}'.format(k, soma))
