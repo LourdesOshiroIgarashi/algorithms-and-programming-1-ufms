e15e0141bbac04fdb3f8433135c6f345be7b6835
lutzzdias <80894260+lutzzdias@users.noreply.github.com>
2021-06-14 21:56:02 -0300

    tirei as variáveis inúteis q tinha

--- Lists/Estrutura_de_Repetições_Aninhadas/ThiagoD/07.py
+++ Lists/Estrutura_de_Repetições_Aninhadas/ThiagoD/07.py
@@ -1,13 +1,10 @@
 n = int(input())
 
-lista = ["+"]
-x = "+"
-aux = n
 # controla as linhas
 for linha in range(1, n + 1):
-    aux -= 1
-    for i in range(aux, 0, -1):
+    n -= 1
+    for i in range(n, 0, -1):
         print(" ", end="")
     for j in range(linha):
-        print(x, end=" ")
+        print("+", end=" ")
     print("")
